390ae006a099ec4ce62a835f74c262db1c3a32d4
wsdamieno <wsdamieno@gmail.com>
2023-11-28 14:25:47 +0100

    update

---
+++ Castalia/src/node/application/leachTest/LeachTest.cc
@@ -0,0 +1,121 @@
+/****************************************************************************
+ *  Copyright: National ICT Australia,  2007 - 2011                         *
+ *  Developed at the ATP lab, Networked Systems research theme              *
+ *  Author(s): Athanassios Boulis, Yuriy Tselishchev                        *
+ *  This file is distributed under the terms in the attached LICENSE file.  *
+ *  If you do not find this file, copies can be found by writing to:        *
+ *                                                                          *
+ *      NICTA, Locked Bag 9013, Alexandria, NSW 1435, Australia             *
+ *      Attention:  License Inquiry.                                        *
+ *                                                                          *
+ ****************************************************************************/
+
+#include "LeachTest.h"
+
+Define_Module(LeachTest);
+
+void LeachTest::startup()
+{
+	packet_rate = par("packet_rate");
+	recipientAddress = par("nextRecipient").stringValue();
+	recipientId = atoi(recipientAddress.c_str());
+	startupDelay = par("startupDelay");
+	delayLimit = par("delayLimit");
+	packet_spacing = packet_rate > 0 ? 1 / float (packet_rate) : -1;
+	dataSN = 0;
+	
+	numNodes = getParentModule()->getParentModule()->par("numNodes");
+	packetsSent.clear();
+	packetsReceived.clear();
+	bytesReceived.clear();
+	
+	if (packet_spacing > 0 && recipientAddress.compare(SELF_NETWORK_ADDRESS) != 0)
+		setTimer(SEND_PACKET, packet_spacing + startupDelay);
+	else
+		trace() << "Not sending packets";
+
+	declareOutput("Packets received per node");
+}
+
+void LeachTest::fromNetworkLayer(ApplicationPacket * rcvPacket,
+		const char *source, double rssi, double lqi)
+{
+	int sequenceNumber = rcvPacket->getSequenceNumber();
+	int sourceId = atoi(source);
+	// This node is the final recipient for the packet
+	if (recipientAddress.compare(SELF_NETWORK_ADDRESS) == 0) {
+		if (delayLimit == 0 || (simTime() - rcvPacket->getCreationTime()) <= delayLimit) {
+			trace() << "[LEACH TEST] Received packet #" << sequenceNumber << " from source " << source << " ";
+			collectOutput("Packets received per node", sourceId);
+			packetsReceived[sourceId]++;
+			bytesReceived[sourceId] += rcvPacket->getByteLength();
+		} else {
+			trace() << "Packet #" << sequenceNumber << " from node " << source <<
+				" exceeded delay limit of " << delayLimit << "s";
+		}
+	// Packet has to be forwarded to the next hop recipient
+	} else {
+		ApplicationPacket* fwdPacket = rcvPacket->dup();
+		// Reset the size of the packet, otherwise the app overhead will keep adding on
+		fwdPacket->setByteLength(0);
+		toNetworkLayer(fwdPacket, recipientAddress.c_str());
+	}
+}
+
+void LeachTest::timerFiredCallback(int index)
+{
+	switch (index) {
+		case SEND_PACKET:{
+			trace() << "Sending packet #" << dataSN;
+			toNetworkLayer(createGenericDataPacket(0, dataSN), recipientAddress.c_str());
+			packetsSent[recipientId]++;
+			dataSN++;
+			setTimer(SEND_PACKET, packet_spacing);
+			break;
+		}
+	}
+}
+
+// This method processes a received carrier sense interupt. Used only for demo purposes
+// in some simulations. Feel free to comment out the trace command.
+void LeachTest::handleRadioControlMessage(RadioControlMessage *radioMsg)
+{
+	switch (radioMsg->getRadioControlMessageKind()) {
+		case CARRIER_SENSE_INTERRUPT:
+			trace() << "CS Interrupt received! current RSSI value is: " << radioModule->readRSSI();
+                        break;
+	}
+}
+
+void LeachTest::finishSpecific() {
+	declareOutput("Packets reception rate");
+	declareOutput("Packets loss rate");
+
+	cTopology *topo;	// temp variable to access packets received by other nodes
+	topo = new cTopology("topo");
+	topo->extractByNedTypeName(cStringTokenizer("node.Node").asVector());
+
+	long bytesDelivered = 0;
+	for (int i = 0; i < numNodes; i++) {
+		LeachTest *appModule = dynamic_cast<LeachTest*>
+			(topo->getNode(i)->getModule()->getSubmodule("Application"));
+		if (appModule) {
+			int packetsSent = appModule->getPacketsSent(self);
+			if (packetsSent > 0) { // this node sent us some packets
+				float rate = (float)packetsReceived[i]/packetsSent;
+				collectOutput("Packets reception rate", i, "total", rate);
+				collectOutput("Packets loss rate", i, "total", 1-rate);
+				trace() << "PACKET LOSS: El nodo " << i << " ha mandado " << packetsSent << " paquetes, y el sink ha recibido " << packetsReceived[i] << " de ellos";
+			}
+
+			bytesDelivered += appModule->getBytesReceived(self);
+		}
+	}
+	delete(topo);
+
+	if (packet_rate > 0 && bytesDelivered > 0) {
+		double energy = (resMgrModule->getSpentEnergy() * 1000000000)/(bytesDelivered * 8);	//in nanojoules/bit
+		declareOutput("Energy nJ/bit");
+		collectOutput("Energy nJ/bit","",energy);
+	}
+}
---
+++ Castalia/src/node/communication/routing/leachRouting/LeachRouting.h
@@ -0,0 +1,167 @@
+//****************************************************************************
+//*  Copyright (c) Federal University of Para, brazil - 2011                 *
+//*  Developed at the Research Group on Computer Network and Multimedia      *
+//*  Communication (GERCOM)     				             *
+//*  All rights reserved    				                     *
+//*                                                                          *
+//*  Permission to use, copy, modify, and distribute this protocol and its   *
+//*  documentation for any purpose, without fee, and without written         *
+//*  agreement is hereby granted, provided that the above copyright notice,  *
+//*  and the author appear in all copies of this protocol.                   *
+//*                                                                          *
+//*  Module:   LEACH Clustering Protocol for Castalia Simulator              *
+//*  Version:  0.2                                                           *
+//*  Author(s): Adonias Pires <adonias@ufpa.br>                              *
+//*             Claudio Silva <claudio.silva@itec.ufpa.br>                   *
+//****************************************************************************/
+
+#ifndef _LEACHROUTING_H_
+#define _LEACHROUTING_H_
+
+#include <queue>
+#include <vector>
+#include <omnetpp.h>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <sstream>
+#include "VirtualRouting.h"
+#include "VirtualApplication.h"	
+#include "LeachRoutingPacket_m.h"
+#include "ApplicationPacket_m.h"
+#include "NoMobilityManager.h"
+
+
+using namespace std;
+
+enum LeachRoutingTimers {
+	START_ROUND = 1,	
+	SEND_ADV = 2,	
+	JOIN_CH = 3,		
+	MAKE_TDMA = 4,			
+	START_SLOT = 5,	
+	END_SLOT = 6,
+	DELAY_BETWEEN_PACKETS = 7,	// XXX-albarc
+	COUNTER_ENERGY_MONITORING = 8,	// XXX-albarc
+};
+
+struct CHInfo
+{
+	int src;
+	double rssi;
+};
+
+class LeachRouting : public VirtualRouting {
+
+private:
+	
+	string applicationID;
+	int advPacketSize;
+	int tdmaPacketSize;
+	int dataPacketSize;
+	int joinPacketSize;
+
+	double maxPower;
+	double sensibility;
+	double aggrConsumption;
+	
+	double slotLength;
+	int clusterLength;
+	double percentage;
+	double probability;
+	double roundLength;
+	int roundNumber;
+	int dataSN;
+	
+	bool isCH;
+	bool isSink;
+	bool isCt;
+	bool endFormClus;
+
+	vector<RoutingPacket> bufferAggregate;
+	vector<int> powers;
+	queue <cPacket *> tempTXBuffer;
+	vector <int> clusterMembers;
+	list <CHInfo> CHcandidates;
+
+	/* XXX-albarc Añadir slots extra para sources prioritarias */
+	int sizeSchedule;
+
+	/* XXX-albarc 18/07/17 Slots extra determinados por el campo de nivel de prioridad en el paquete de JOIN */
+	double slotsIncreasePercentagePriorityLevel_1;
+	double slotsIncreasePercentagePriorityLevel_2;
+	vector<int> priorityLevelsVector; // Se rellenará por los CH al ir recibiendo mensajes de JOIN
+	vector<int> TDMATable; // Se rellenará por los CH teniendo en cuenta los clusterMembers y sus diferentes niveles de prioridad
+	int myPriorityLevel; // El propio de cada nodo, que incorporará en sus paquetes de JOIN
+
+	// Para los collectOutput, ya que el sink donde se desagregan no tiene la información de los paquetes de JOIN
+	const char *priorityLevel_1_Sources;
+	vector<int> priorityLevel_1_SourcesVector;
+	const char *priorityLevel_2_Sources;
+	vector<int> priorityLevel_2_SourcesVector;
+	int totalPriorityLevel_1_PacketsReceived;
+	int totalPriorityLevel_2_PacketsReceived;
+
+	// XXX-albarc Para no mandar todos los paquetes en el mismo momento y evitar colisiones
+	bool delayBetweenPacketsEnabled;
+	double delayBetweenPackets;
+
+	// XXX-albarc Para calcular las medias de recibidos al final
+	int totalPacketsReceived;
+	int totalRegularPacketsReceived;
+	int numNodes;
+
+	// XXX-albarc Para calcular la latencia según prioridad
+	double totalLatencyOfAllPackets;
+	double totalLatencyOfPL1Packets;
+	double totalLatencyOfPL2Packets;
+	double totalLatencyOfRegularPackets;
+
+	double totalLatencyOfNode43Packets;
+	double totalNode43PacketsReceived;
+
+	// Histograma de paquetes recibidos por buckets de tiempo
+	int simTimeLimit;
+	int numTimeBuckets;
+	int timeBucketDuration;
+	double energyConsumedInTimeBucket;
+	int currentTimeBucket;
+	ResourceManager* resourceModule;
+
+	// PRUEBA CALCULAR MINISLOTS
+	int slot;
+	int miniSlot;
+	int totalPacketsBuffered;
+	vector<int> mySlotsVector;
+
+protected:
+
+	void startup();
+	void fromApplicationLayer(cPacket *, const char *);
+	void fromMacLayer(cPacket *, int, double, double);
+	void timerFiredCallback(int);
+	void processBufferedPacket();
+	
+	void sendAggregate();
+	void setPowerLevel(double);
+	void setStateSleep();
+	void setStateRx();
+	void levelTxPower(int);	
+	void readXMLparams();
+	
+	void processBufferedPacketWithoutSending();	// XXX-albarc	
+	void sendSinglePacket();	// XXX-albarc
+
+	void finishSpecific();	// XXX-albarc
+	int distanceToNextSlot();	// XXX-albarc
+
+	int populateTDMATable();	// XXX-albarc
+	bool isPriorityLevelSource(int, int);	// XXX-albarc
+
+};
+bool cmpRssi(CHInfo a, CHInfo b);
+
+#endif			
